ccc92de6e8a46f40b100184dc51062b70b26673c
NikolasMelui <slak@samaradom.ru>
2018-07-12 10:04:52 +0400

    Add new code in index.js main file. Add new package.json information.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import http from 'http';
 
-const server = http.createServer((req, res) => res.end('Welcome to multikey-node-boilerplate!'));
-
-server.listen(3000, () => global.console.log(`Server is listening on port 3000`));
-
-import http from 'http';
-
 const config = {
-	port: 3000,
+	port: Number(process.argv[2]) || 8080,
 };
 
 http.createServer((req, res) => {
-	global.console.log();
+	global.console.log(req.url);
 	res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
 	res.end(`Hello from port: ${config.port} and welcome to multikey-node-boilerplate!`);
 }).listen(config.port, () => global.console.log(`Server is listening on port ${config.port}`));
