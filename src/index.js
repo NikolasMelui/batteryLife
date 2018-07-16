af115f1c0e2e9e8ac56850be3886059c76dac9e3
NikolasMelui <slak@samaradom.ru>
2018-07-16 22:17:53 +0400

    Promisify all async code.

--- src/index.js
+++ src/index.js
@@ -5,18 +5,27 @@ const config = {
 	port: Number(process.argv[2]) || 3000,
 };
 
-const execComand = (res, command) => {
-	child_process.exec(command, (err, stdout, stderr) => {
-		if (err) {
-			console.error(`Child process failed with error code ${err.code}\nStderr: ${stderr}`);
-		} else {
-			res.writeHead(200, { 'Content-Type': 'application/json' });
-			res.write(JSON.stringify({ 'Battery life': stdout }));
-			res.end();
-		}
+const execComand = command => {
+	return new Promise((resolve, reject) => {
+		child_process.exec(command, (err, stdout, stderr) => {
+			if (err) {
+				global.console.error(`Child process failed with error code ${err.code}\nStderr: ${stderr}`);
+				resolve(err);
+			} else {
+				reject(stdout);
+			}
+		});
 	});
 };
 
 http.createServer((req, res) => {
-	execComand(res, 'pmset -g batt | egrep "([0-9]+%).*" -o');
+	execComand('pmset -g batt | egrep "([0-9]+%).*" -o')
+		.then(response => {
+			res.write(JSON.stringify({ 'Battery life': response }));
+			res.end();
+		})
+		.catch(error => {
+			res.writeHead(500, { 'Content-Type': 'application/json' });
+			res.end(error);
+		});
 }).listen(config.port, () => global.console.log(`Server is listening on port ${config.port}`));
